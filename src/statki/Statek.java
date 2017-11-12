a3df8af86baadb4b37fbe23249b8f66ef13743fa
okuf <okuf@okuf-Lenovo-ideapad-320-15IKB>
2017-11-12 23:08:27 +0100

    Zadklarowanie klas i nic wiecej

---
+++ src/statki/Statek.java
@@ -0,0 +1,5 @@
+package statki;
+
+public class Statek {
+
+}
---
+++ src/statki/gra.java
@@ -0,0 +1,10 @@
+package statki;
+
+public class gra {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
