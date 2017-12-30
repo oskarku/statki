665c38862057a5d369c34a48b8fab46e3a3ffa0a
kob <kob@DESKTOP-8PSC6SM>
2017-12-30 14:11:03 +0100

    versja 0.01

--- src/statki/gra.java
+++ src/statki/gra.java
@@ -8,33 +8,40 @@ import java.util.List;
 public class gra {
 
 	public static void main(String[] args) {
-		DaneStatkow lol;
-		lol= new DaneStatkow();
+		DaneStatkow lol= new DaneStatkow();
 		Flota s1, s2;
-		s1 = new Flota("flota_1.txt","Flota gracza nr 1");
-		s2 = new Flota("flota_2.txt", "Flota gracza nr 2");
-		/*s1.flotaa.get(0).statystyki();
-		s2.flotaa.get(0).statystyki();
-		s2.flotaa.get(0).strzel(s1.flotaa.get(0));
-		s1.flotaa.get(0).trafiony(s2.flotaa.get(0));
-		System.out.println(" Po walce");
-		s1.flotaa.get(0).statystyki();
-		s2.flotaa.get(0).statystyki();*/
-		//s1.wybierz_cel(s2);
-		//s2.wybierz_cel(s1);
+		s1 = new Flota("flota_1.txt","Gracz nr 1");
+		s2 = new Flota("flota_2.txt", "Gracz nr 2");
 		
 		for (int i=0; i<6; i++) {
-			System.out.println(s1.flotaa.size()+" "+s2.flotaa.size());
-			if(s1.flotaa.size()>0 && s2.flotaa.size()>0) {
-			s1.wybierz_cel(s2);
-			if(!s2.flotaa.isEmpty())s2.usun_statki();
-			s2.wybierz_cel(s1);
-			if(!s1.flotaa.isEmpty())s1.usun_statki();}
-			break;
+			System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> RUNDA NR "+(i+1)+" <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<,");
+			System.out.println("Ilosc statkow floty 1: "+s1.flotaa.size()+" ilosc statkow floty 2 : "+s2.flotaa.size());
+			if(!s1.flotaa.isEmpty() && !s2.flotaa.isEmpty()) {
+				System.out.println(s1.NazwaFloty+ "wybiera cel z floty "+s2.NazwaFloty);
+				s1.wybierz_cel(s2);
+				if(!s2.flotaa.isEmpty())s2.usun_statki();
+				//System.out.println("stan floty 2 "+s2.aktualny_stan());
+				System.out.println(s2.NazwaFloty+" wybiera cel z "+s1.NazwaFloty);
+				s2.wybierz_cel(s1);
+				if(!s1.flotaa.isEmpty())s1.usun_statki();
+				//System.out.println("Stan floty 1"+s1.aktualny_stan());
+				}
+			
+		
 		}
-		if (s1.flotaa.size()>s2.flotaa.size())System.out.print("wygrala flota: "+s1.NazwaFloty);
-		else if (s2.flotaa.size()>s1.flotaa.size())System.out.println("wygrala flota: "+s2.NazwaFloty);
+		if (s1.flotaa.size()>s2.flotaa.size()) {
+			System.out.print("wygrala flota: "+s1.NazwaFloty);
+			s1.aktualny_stan();
+		}
+		else if (s2.flotaa.size()>s1.flotaa.size()) {
+			System.out.println("wygrala flota: "+s2.NazwaFloty);
+			s2.aktualny_stan();
+		}
 		else System.out.println("jest remis");
+		System.out.println("Aktualny stan floty1");
+		s1.aktualny_stan();
+		System.out.println("Aktualny stan floty2");
+		s2.aktualny_stan();
 		
 		
 		
